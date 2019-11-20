69e9b578120c49e324ede93c6908806761957292
Pavel-Novoselov <npe@inbox.ru>
2019-11-20 11:24:41 +0300

    преподавательская версия после урока2

--- core/src/com/star/app/game/Background.java
+++ core/src/com/star/app/game/Background.java
@@ -1,9 +1,11 @@
-package com.star.game;
+package com.star.app.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.star.app.StarGame;
+import com.star.app.screen.ScreenManager;
 
 public class Background {
     private class Star {
@@ -12,30 +14,31 @@ public class Background {
         private float scale;
 
         public Star() {
-            this.position = new Vector2(MathUtils.random(0, ScreenManager.SCREEN_WIDTH), MathUtils.random(-200, ScreenManager.SCREEN_HEIGHT + 200));
-            this.velocity = new Vector2(MathUtils.random(-40, -5), 0);
-            this.scale = Math.abs(velocity.x) / 40.0f * 0.7f;
+            this.position = new Vector2(MathUtils.random(-200, ScreenManager.SCREEN_WIDTH + 200), MathUtils.random(-200, ScreenManager.SCREEN_HEIGHT + 200));
+            this.velocity = new Vector2(MathUtils.random(-15, -1), 0);
+            this.scale = Math.abs(velocity.x) / 15.0f * 0.7f;
         }
 
         public void update(float dt) {
-            position.x += (velocity.x - game.getHero().getLastDisplacement().x * 15) * dt;
-            position.y += (velocity.y - game.getHero().getLastDisplacement().y * 15) * dt;
-            if (position.x < -20) {
+            position.x += (velocity.x - gc.getHero().getVelocity().x / 10.0f) * dt;
+            position.y += (velocity.y - gc.getHero().getVelocity().y / 10.0f) * dt;
+            if (position.x < -200) {
                 position.x = ScreenManager.SCREEN_WIDTH + 20;
                 position.y = MathUtils.random(-200, ScreenManager.SCREEN_HEIGHT + 200);
-                scale = Math.abs(velocity.x) / 40.0f * 0.7f;
+                velocity.x = MathUtils.random(-15, -1);
+                scale = Math.abs(velocity.x) / 15.0f * 0.7f;
             }
         }
     }
 
     private final int STARS_COUNT = 600;
-    private StarGame game;
+    private GameController gc;
     private Texture textureCosmos;
     private Texture textureStar;
     private Star[] stars;
 
-    public Background(StarGame game) {
-        this.game = game;
+    public Background(GameController gc) {
+        this.gc = gc;
         this.textureCosmos = new Texture("bg.png");
         this.textureStar = new Texture("star16.png");
         this.stars = new Star[STARS_COUNT];
