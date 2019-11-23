1506cb121e76c3c3d577bfda1525fda0838b3b86
Pavel-Novoselov <npe@inbox.ru>
2019-11-23 19:18:25 +0300

    преподавательская версия после урока 3

--- core/src/com/star/app/game/GameController.java
+++ core/src/com/star/app/game/GameController.java
@@ -1,5 +1,8 @@
 package com.star.app.game;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.star.app.screen.ScreenManager;
+
 public class GameController {
     private Background background;
     private BulletController bulletController;
@@ -26,8 +29,11 @@ public class GameController {
         this.background = new Background(this);
         this.hero = new Hero(this);
         this.bulletController = new BulletController();
-        this.asteroidController = new AsteroidController();
- //       asteroidController.setup();
+        this.asteroidController = new AsteroidController(this);
+        for (int i = 0; i < 3; i++) {
+            this.asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH), MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
+                    MathUtils.random(-150.0f, 150.0f), MathUtils.random(-150.0f, 150.0f), 1.0f);
+        }
     }
 
     public void update(float dt) {
@@ -38,26 +44,22 @@ public class GameController {
         checkCollisions();
     }
 
-    // попадание в астероид (ДЗ)
+    // попадание в астероид пули
     public void checkCollisions() {
         //проверка попадания в астероид
         for (int i = 0; i < bulletController.getActiveList().size(); i++) {
             Bullet b = bulletController.getActiveList().get(i);
             for (int j = 0; j < asteroidController.getActiveList().size(); j++) {
                 Asteroid a = asteroidController.getActiveList().get(j);
-                if (a.getPosition().dst(b.getPosition()) < 128.0f) { // 128.0f - примерно диаметр астероида
-                    a.deactivate();
-                    // считаем что попали,  астероид уничножен
+                if (a.getHitArea().contains(b.getPosition())) {
+                    b.deactivate();
+                    if (a.takeDamage(1)) {
+                        hero.addScore(a.getHpMax() * 100);
+                    }
+                    break;
                 }
             }
         }
-        //проверка столкновения астероида и корабля
-        for (int i = 0; i < asteroidController.getActiveList().size(); i++) {
-            Asteroid a = asteroidController.getActiveList().get(i);
-            if (a.getPosition().dst(hero.getPosition()) < 128.0f) { // 128.0f - примерно диаметр астероида
-                    a.deactivate();
-                    // считаем что столкнулись, астероид уничножен
-            }
-        }
+  
     }
 }
